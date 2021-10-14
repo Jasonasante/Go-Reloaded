b97dfd6fcfb120af01d7cf0f2b4e99576e02e74c
jasonasante <jason.asante@outlook.com>
2021-10-14 18:26:09 +0100

    go-reloaded

--- go-reloaded.go
+++ go-reloaded.go
@@ -62,20 +62,8 @@ func Capitalize(s string) string {
 	// at r[i+1], and since there is no r[i+1] at the end the code will produce an error.
 	return string(cap)
 }
-
-// func Hex() {
-// }
-
-func main() {
-	fileName := "sample.txt"
-	content, err := ioutil.ReadFile(fileName)
-	// this reads the content of the file
-	if err != nil {
-		fmt.Printf("the mistake is : %v\n", err.Error())
-	}
-	sampleString := string(content)
-	// this turns the contents of the file into a string.
-	splitString := strings.Split(sampleString, " ")
+func aToAn (s string)[]string{
+	splitString := strings.Split(s, " ")
 	// this splits the string into its own elements by the space.
 	for i, ele := range splitString {
 		// the following code is to convert a/A into an/An
@@ -106,6 +94,54 @@ func main() {
 				}
 			}
 			// the above code was to change a/A to an/An.
+		}
+	}	
+}
+
+// func Hex() {
+	
+// }
+
+func main() {
+	fileName := "sample.txt"
+	content, err := ioutil.ReadFile(fileName)
+	// this reads the content of the file
+	if err != nil {
+		fmt.Printf("the mistake is : %v\n", err.Error())
+	}
+	sampleString := string(content)
+	// this turns the contents of the file into a string.
+	// splitString := strings.Split(sampleString, " ")
+	// // this splits the string into its own elements by the space.
+	// for i, ele := range splitString {
+	// 	// the following code is to convert a/A into an/An
+	// 	if i >= 0 && i < len(splitString)-1 {
+	// 		// since i'm dealing with i+1, i dont want to go outside of the range so i the
+	// 		// the last the index the code should consider is the penultimate index hence
+	// 		// i<len(splitString-1 which is equivalent to i==len(splitString)-2)
+	// 		if strings.HasPrefix(splitString[i+1], "a") || strings.HasPrefix(splitString[i+1], "e") || strings.HasPrefix(splitString[i+1], "i") || strings.HasPrefix(splitString[i+1], "o") || strings.HasPrefix(splitString[i+1], "u") || strings.HasPrefix(splitString[i+1], "h") {
+	// 			// strings.HasPrefix checks if the string starts with the substring
+	// 			// the ele in splitString[i] is the string and "a" is the substring.
+	// 			// then repeat for all lowercase vowels and h.
+	// 			if ele == "a" {
+	// 				splitString[i] = "an"
+	// 			}
+	// 			if ele == "A" {
+	// 				splitString[i] = "An"
+	// 			}
+	// 		}
+	// 		if strings.HasPrefix(splitString[i+1], "A") || strings.HasPrefix(splitString[i+1], "E") || strings.HasPrefix(splitString[i+1], "I") || strings.HasPrefix(splitString[i+1], "O") || strings.HasPrefix(splitString[i+1], "U") || strings.HasPrefix(splitString[i+1], "H") {
+	// 			// strings.HasPrefix checks if the string starts with the substring
+	// 			// the ele in splitString[i] is the string and "a" is the substring.
+	// 			// then repeat for all uppercase vowels and H.
+	// 			if ele == "a" {
+	// 				splitString[i] = "an"
+	// 			}
+	// 			if ele == "A" {
+	// 				splitString[i] = "An"
+	// 			}
+	// 		}
+	// 		// the above code was to change a/A to an/An.
 
 			if strings.HasPrefix(splitString[i+1], "(cap") {
 				splitString[i] = Capitalize(splitString[i])
@@ -130,5 +166,9 @@ func main() {
 	// 		splitString[i] = strings.Trim(splitString[i], splitString[i])
 	// 	}
 	// }
+	var newString string
+	for a :=range splitString{
+		newString= append(newString,splitString[a])
+	}
 	fmt.Println(splitString)
 }
